4116d9558392b188cc46ee67da0e32be095d1671
mostafa-ti <mostafa.torbati@gmail.com>
2023-01-16 11:23:43 +0100

    count Bash scripts added

---
+++ Cellranger_count/DG3_count.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+#SBATCH -A LSENS2018-3-3
+#SBATCH -n 20
+#SBATCH -N 1
+#SBATCH -t 24:00:00
+#SBATCH -J Run_DG3_count
+#SBATCH -o DG3count%j.out
+#SBATCH -e DG3count%j.err
+#SBATCH -p dell
+module --force purge
+module load SoftwareTree/Haswell
+module load cellranger/6.0.0
+cellranger count --id=DG3_count \
+--fastqs=/projects/fs1/mtorbati/Torbati/CellRanger_V7/mkfastq/firstRun/mkfastq_1stRun/outs/fastq_path/Chromium_20180912,/projects/fs1/mtorbati/Torbati/CellRanger_V7/mkfastq/secondRun/mkfastq_2ndRun/outs/fastq_path/Chromium_20180912 \
+--sample=DG3 \
+--transcriptome=/projects/fs1/mtorbati/Torbati/cellranger_custom_RefGen/custom_ref_gene/Mus.musculus_genome_EGFP
+exit 0
+
---
+++ Cellranger_count/OB24_count.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+#SBATCH -A LSENS2018-3-3
+#SBATCH -n 20
+#SBATCH -N 1
+#SBATCH -t 24:00:00
+#SBATCH -J Run_OB24_count
+#SBATCH -o OB24count%j.out
+#SBATCH -e OB24count%j.err
+#SBATCH -p dell
+module --force purge
+module load SoftwareTree/Haswell
+module load cellranger/6.0.0
+cellranger count --id=OB24_count \
+--fastqs=/projects/fs1/mtorbati/Torbati/CellRanger_V7/mkfastq/firstRun/mkfastq_1stRun/outs/fastq_path/Chromium_20180912,/projects/fs1/mtorbati/Torbati/CellRanger_V7/mkfastq/secondRun/mkfastq_2ndRun/outs/fastq_path/Chromium_20180912 \
+--sample=OB24 \
+--transcriptome=/projects/fs1/mtorbati/Torbati/cellranger_custom_RefGen/custom_ref_gene/Mus.musculus_genome_EGFP
+exit 0
